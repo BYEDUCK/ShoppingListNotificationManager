2b282b21d67e4e555bccec820f40f86b655c9d64
Mateusz <s23667@pjwstk.edu.pl>
2020-11-27 20:02:14 +0100

    init

---
+++ app/src/main/java/com/byeduck/shoppinglistnotificationprovider/ModificationEventReceiver.kt
@@ -0,0 +1,13 @@
+package com.byeduck.shoppinglistnotificationprovider
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class ModificationEventReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("ModificationEventReceiver.onReceive() is not implemented")
+    }
+}
