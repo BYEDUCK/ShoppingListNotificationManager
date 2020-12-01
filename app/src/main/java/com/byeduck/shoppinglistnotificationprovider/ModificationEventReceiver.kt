a2ffc154b3f32fda79e4860145d778155a2d57e0
Mateusz <s23667@pjwstk.edu.pl>
2020-12-01 22:25:30 +0100

    working!

--- app/src/main/java/com/byeduck/shoppinglistnotificationprovider/ModificationEventReceiver.kt
+++ app/src/main/java/com/byeduck/shoppinglistnotificationprovider/ModificationEventReceiver.kt
@@ -3,14 +3,21 @@ package com.byeduck.shoppinglistnotificationprovider
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 class ModificationEventReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        val serviceIntent = Intent(context, NotificationService::class.java)
+        Toast.makeText(context, "Received broadcast", Toast.LENGTH_SHORT).show()
         val listId = intent.getLongExtra("listId", -1L)
-        serviceIntent.putExtra("listId", listId)
-        context.startService(serviceIntent)
+        val elemTxt = intent.getStringExtra("elemTxt")
+        val elemId = intent.getLongExtra("elemId", -1L)
+        val serviceIntent = Intent(context, NotificationService::class.java).apply {
+            putExtra("listId", listId)
+            putExtra("elemTxt", elemTxt)
+            putExtra("elemId", elemId)
+        }
+        context.startForegroundService(serviceIntent)
     }
 
 }
--- app/src/main/java/com/byeduck/shoppinglistnotificationprovider/NotificationService.kt
+++ app/src/main/java/com/byeduck/shoppinglistnotificationprovider/NotificationService.kt
@@ -2,13 +2,16 @@ package com.byeduck.shoppinglistnotificationprovider
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_CANCEL_CURRENT
 import android.app.Service
+import android.content.ComponentName
 import android.content.Intent
 import android.os.Binder
-import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import java.util.*
 
 class NotificationService : Service() {
 
@@ -19,29 +22,43 @@ class NotificationService : Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        createNotificationChannel()
+        val channelId = createNotificationChannel()
         val listId = intent?.extras?.getLong("listId", -1L) ?: -1L
-        val notification = NotificationCompat.Builder(this, getString(R.string.channelId))
-                .setContentTitle(getString(R.string.notificationTitle))
-                .setContentText("Go to shopping list $listId")
-                .setAutoCancel(true)
-                .build()
+        val elemTxt = intent?.extras?.getString("elemTxt") ?: ""
+        val elemId = intent?.extras?.getLong("elemId") ?: -1L
+        val activityIntent = Intent("${getString(R.string.mainPackage)}.ADD_EDIT_LIST").apply {
+            putExtra("listId", listId)
+            putExtra("elemId", elemId)
+            component = ComponentName(
+                getString(R.string.mainPackage),
+                "${getString(R.string.mainPackage)}.detail.AddEditShoppingElementActivity"
+            )
+        }
+        val pendingIntent = PendingIntent.getActivity(
+            this, System.currentTimeMillis().toInt(), activityIntent, FLAG_CANCEL_CURRENT
+        )
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("Shopping list $listId updated")
+            .setContentText("Element($elemId) \"$elemTxt\" has been added. Edit it!")
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .build()
         val notificationManager = NotificationManagerCompat.from(this)
         notificationManager.notify(notificationId++, notification)
         return super.onStartCommand(intent, flags, startId)
     }
 
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
-            return
-        }
+    private fun createNotificationChannel(): String {
+        val channelId = UUID.randomUUID().toString()
         val notificationChannel = NotificationChannel(
-                getString(R.string.channelId),
-                getString(R.string.channelName),
-                NotificationManager.IMPORTANCE_DEFAULT
+            channelId,
+            getString(R.string.channelName),
+            NotificationManager.IMPORTANCE_DEFAULT
         )
         notificationChannel.description = getString(R.string.channelDescription)
         val notificationManager = NotificationManagerCompat.from(this)
         notificationManager.createNotificationChannel(notificationChannel)
+        return channelId
     }
 }
